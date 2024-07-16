c285bc1b04937c2036faeb6d4473e680f1a5ae8f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-16 22:46:32 +0200

    Added an api key.

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,9 @@
 const api = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13'
-
+const apiKey = "f56de56508mshd0bfd5041a309a9p1b173djsnd3577aa851d9"
 
 async function getTheData () {
     try {
-        const res = await fetch(api)
+        const res = await fetch(api + `&appid=${apiKey}`)
         const data = await res.json()
         console.log(data)
 
