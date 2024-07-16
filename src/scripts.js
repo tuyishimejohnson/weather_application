2b4c97cae9a1f73bcf312d21f43e08706b668123
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-16 23:13:08 +0200

    Got the api of the weather.

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,15 @@
-const api = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13'
-const apiKey = "f56de56508mshd0bfd5041a309a9p1b173djsnd3577aa851d9"
+const api = 'https://open-weather13.p.rapidapi.com/city/Kigali/EN'
+const options = {
+    method: "GET",
+    headers: {
+        'x-rapidapi-key': 'f56de56508mshd0bfd5041a309a9p1b173djsnd3577aa851d9',
+		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+    }
+}
 
 async function getTheData () {
     try {
-        const res = await fetch(api + `&appid=${apiKey}`)
+        const res = await fetch(api, options)
         const data = await res.json()
         console.log(data)
 
